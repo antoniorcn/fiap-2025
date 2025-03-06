b07d1f83d2c3e551a1d90dcb3930b3bcd19072cf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-06 09:39:12 -0300

    2tdsph aula 07 parte 2

--- 2tdsph/aula07/agenda_contato/App.tsx
+++ 2tdsph/aula07/agenda_contato/App.tsx
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+let listaPersonagens: Personagem[];
+
+let sage: Personagem = { nome: "Sage", vida: 100 };
+let arana: Personagem = { nome: "Arana", vida: 100, arma: "Ak47" };
+listaPersonagens = [sage, arana, { nome: "pimenta", vida: 85 }];
+listaPersonagens.push({ nome: "Alberto", vida: 78 });
+
+
+
 export default function App() {
+  // const listaElementos = []
+  // for (let i = 0; i < listaPersonagens.length; i++) {
+  //   const personagem = listaPersonagens[i];
+  //   listaElementos.push(<Text>Nome: {personagem.nome}</Text>)
+  // }
+
+  // for (let i in listaPersonagens) {
+  //   const personagem = listaPersonagens[i];
+  //   listaElementos.push(<Text>{personagem.nome}</Text>)
+  // }
+
+  // for (let personagem of listaPersonagens) {
+  //   listaElementos.push(<Text>Nome: {personagem.nome}</Text>)
+  // }
+
+  const listaElementos = listaPersonagens.map(
+    (personagem: Personagem) => {
+      return (<Text>
+        Programação funcional: {personagem.nome}
+      </Text>)
+    }
+  )
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {listaElementos}
     </View>
   );
 }
