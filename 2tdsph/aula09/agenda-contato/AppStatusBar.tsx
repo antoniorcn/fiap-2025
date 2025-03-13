c043a5e8508dbdea6d9b80af048c346d46b3a3f4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-13 09:50:06 -0300

    2tdsph aula09 parte 2

--- 2tdsph/aula09/agenda-contato/AppStatusBar.tsx
+++ 2tdsph/aula09/agenda-contato/AppStatusBar.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Button, ColorValue, FlatList, ListRenderItemInfo, Text, TextInput, View } from 'react-native';
+import { Button, ColorValue, Modal, StatusBar, Text, TextInput, View } from 'react-native';
 interface FormularioProps { 
   versao : string
   titulo? : string
 }
 
-interface Contato { 
-  nome : string
-  telefone : string
-  email : string
-}
 
-const contatos : Contato[] = [
+const contatos = [
   {nome: "Joao", telefone: "(11)111", email: "joao@teste.com"},
   {nome: "Maria", telefone: "(11)222", email: "maria@teste.com"},
   {nome: "Jose", telefone: "(11)333", email: "jose@teste.com"},
@@ -41,25 +36,45 @@ interface ContatoItemProps {
   nome : string
   telefone : string
   email : string
+  color : ColorValue
 }
 
-const ContatoItem = ( props : ListRenderItemInfo<Contato> ) => { 
+const ContatoItem = ( props : ContatoItemProps ) => { 
   return (
-    <View>
-      <Text>{props.item.nome}</Text>
-      <Text>{props.item.telefone}</Text>
-      <Text>{props.item.email}</Text>
+    <View style={{backgroundColor: props.color}}>
+      <Text>{props.nome}</Text>
+      <Text>{props.telefone}</Text>
+      <Text>{props.email}</Text>
     </View>
   )
 }
 
+interface ListagemProps { 
+  versao : string
+}
+
+const Listagem = ( props : ListagemProps ) => { 
+  const listaContatosVisuais : React.ReactElement[] = contatos.map( 
+    ( elemento ) => { 
+      return (
+        <ContatoItem nome={elemento.nome} email={elemento.email} 
+            telefone={elemento.telefone} />
+      );
+    }
+  );
+  return ( 
+    <View>
+      <Text>Agenda de contato {props.versao}</Text>
+      {listaContatosVisuais}
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View>
-      <FlatList data={contatos} renderItem={ContatoItem}
-        keyExtractor={( item : Contato )=>item.nome}
-      />
+      {/* <StatusBar backgroundColor={"red"}/> */}
+      <Listagem versao="1.1" />
     </View>
   );
 }
