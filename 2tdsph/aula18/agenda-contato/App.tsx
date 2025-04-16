abff98d0d87c676566968c4f2bbed1540e65e04b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-16 20:48:35 -0300

    2tdspv aula18

--- 2tdsph/aula18/agenda-contato/App.tsx
+++ 2tdsph/aula18/agenda-contato/App.tsx
@@ -1,38 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, ParamListBase } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { useState } from 'react';
+import { ContatoModulo } from './ContatoScreen';
+import { Contato } from './Contato';
 
 const {Screen, Navigator} = createDrawerNavigator();
-const Tab = createBottomTabNavigator();
-
-const ContatoFormulario = () : React.ReactElement => { 
-  return (
-    <View>
-      <Text> Formulario de Contato </Text>
-    </View>
-  )
-}
-
-const ContatoListagem = () : React.ReactElement => { 
-  return (
-    <View>
-      <Text> Listagem de Contato </Text>
-    </View>
-  )
-}
-
-const ContatoModulo = () : React.ReactElement => {
-  return (
-    <View>
-      <Tab.Navigator>
-        <Tab.Screen name="ContatoFormulario" component={ContatoFormulario}/>
-        <Tab.Screen name="ContatoListagem" component={ContatoListagem}/>
-      </Tab.Navigator>
-    </View>
-  )
-}
 
 const CicloSocialModulo = () : React.ReactElement => {
   return (
@@ -43,11 +17,21 @@ const CicloSocialModulo = () : React.ReactElement => {
 }
 
 export default function App() {
+  const [lista, setLista] = useState<Contato[]>([]);
+
+  const gravar = ( nome : string, telefone : string, email : string) => { 
+    const obj : Contato = {nome, telefone, email};
+    setLista( [ ...lista,  obj ] );
+  }
+
   return (
     <NavigationContainer>
       <View style={styles.container}>
         <Navigator>
-          <Screen name="Contato" component={ContatoModulo}/>
+          <Screen name="Contato">
+            {  ( navProps : ParamListBase )=>
+              <ContatoModulo {...navProps} onGravar={gravar} lista={lista}/> }
+          </Screen>
           <Screen name="CicloSocial" component={CicloSocialModulo}/>
         </Navigator>  
         <StatusBar style="auto" />
@@ -60,7 +44,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
 });
---
+++ 2tdsph/aula18/agenda-contato/Contato.ts
@@ -0,0 +1,8 @@
+interface Contato { 
+    nome : string
+    telefone : string
+    email : string
+}
+
+export {Contato}
+  
