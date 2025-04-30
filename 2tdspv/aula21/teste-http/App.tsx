1269ccdfd7827cbf60d437938ba4f03135e6ca8a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-30 20:53:30 -0300

    2tdpv aula 21 parte 1

--- 2tdspv/aula21/teste-http/App.tsx
+++ 2tdspv/aula21/teste-http/App.tsx
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import axios from 'axios';
+import { Alert, Button, FlatList, ListRenderItemInfo, StyleSheet, Text, TextInput, View } from 'react-native';
+import axios, { AxiosResponse } from 'axios';
 import { useState } from 'react';
 
+interface Contato { 
+  id : string
+  nome : string
+  telefone : string
+  email : string
+}
+const ContatoItem = ( props : ListRenderItemInfo<Contato>) => { 
+  return ( 
+    <View>
+      <Text>{props.item.nome}</Text>
+      <Text>{props.item.telefone}</Text>
+      <Text>{props.item.email}</Text>
+    </View>
+  )
+}
 export default function App() {
   const [nome, setNome] = useState<string>("");
   const [telefone, setTelefone] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+
+  const [lista, setLista] = useState<Contato[]>([]);
 
   return (
     <View style={styles.container}>
@@ -27,6 +44,22 @@ export default function App() {
           Alert.alert("Erro ao gravar o contato");
         })
       }} />
+
+      <Button title="Carregar" onPress={()=>{
+        axios.get("https://tdspv-a8c0e-default-rtdb.firebaseio.com/contatos.json")
+        .then((info : AxiosResponse<any, any>)=>{
+          const listaTemp : Contato[] = [];
+          for (const chave in info.data) { 
+            const c = info.data[chave];
+            const contato : Contato = { id: chave, ...c };
+            listaTemp.push(contato);
+          }
+          setLista( listaTemp );
+        })
+        .catch(()=>{})
+      }}/>
+
+      <FlatList data={lista} renderItem={ContatoItem} />
 
     </View>
   );
