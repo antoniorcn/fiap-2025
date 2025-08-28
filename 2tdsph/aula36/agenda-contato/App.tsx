80ede2e3712b628bc50d8118d6adaa473154d78c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-28 09:35:02 -0300

    2tdsph aula36

---
+++ 2tdsph/aula36/agenda-contato/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { ContatoView } from './view/ContatoView';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Agenda de Contato</Text>
+        <StatusBar style="auto" />
+        <ContatoView/>
+      </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+});
---
+++ 2tdsph/aula36/agenda-contato/fetcher/contatoFetcher.ts
@@ -0,0 +1,61 @@
+import { Contato, ContatoErro } from "../model/Contato";
+import axios, { AxiosResponse } from 'axios';
+const apiBase = axios.create({
+    baseURL: "https://tdsph-ad96c-default-rtdb.firebaseio.com"
+});
+
+interface SalvarCallback { 
+    (sucesso : boolean, mensagem : string, errosCampos? : ContatoErro) : void;
+}
+
+interface LerCallback { 
+    (sucesso : boolean, mensagem : string, lista? : Array<Contato>) : void;
+}
+
+interface ApagarCallback { 
+    (sucesso : boolean, mensagem : string) : void;
+}
+
+interface AtualizarCallback { 
+    (sucesso : boolean, mensagem : string, errosCampos? : ContatoErro) : void;
+}
+
+const contatoFetcherSalvar =
+ (contato : Contato, callback : SalvarCallback ) : void => {
+    apiBase.post( "/contatos.json", contato )
+    .then(()=>callback(true, ""))
+    .catch(( erro : any)=>callback(false, erro))
+}
+
+
+const contatoFetcherApagar =
+ (id : string, callback : ApagarCallback ) : void => {
+    apiBase.delete( `/contatos/${id}.json`)
+    .then(()=>callback(true, ""))
+    .catch(( erro : any)=>callback(false, erro))
+}
+
+const contatoFetcherAtualizar =
+ (id : string, contato : Contato, callback : AtualizarCallback ) : void => {
+    apiBase.put( `/contatos/${id}.json`, contato )
+    .then(()=>callback(true, ""))
+    .catch(( erro : any)=>callback(false, erro))
+}
+
+const contatoFetcherLer = (callback : LerCallback) : void => { 
+    apiBase.get("/contatos.json")
+    .then(( resposta : AxiosResponse<any, any>)=>{
+        const listaContatos = [];
+        for ( const chave in resposta.data ){  
+            const objContato : Contato = resposta.data[chave as keyof Contato];
+            objContato.id = chave;
+            listaContatos.push( objContato );
+        }
+        callback(true, `Foram lidos ${listaContatos.length} contatos`, listaContatos);
+    })
+    .catch((erro : any)=>callback(false, erro))
+}
+
+export {contatoFetcherSalvar, contatoFetcherLer, 
+    contatoFetcherApagar, contatoFetcherAtualizar,
+    SalvarCallback, LerCallback, ApagarCallback, AtualizarCallback}; 
